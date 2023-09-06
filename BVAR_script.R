ed64d0b379cad3778fc334c58d910ef31f0a822f
Tomasz Woźniak <wozniak.tom@pm.me>
2023-09-06 21:05:57 +1000

    3D plot script

--- BVAR_script.R
+++ BVAR_script.R
@@ -59,3 +59,34 @@ plot.ts(run$sigma[,1,])
 plot.ts(run$beta[,2,])
 
 apply(run$beta, 2:3, mean)
+
+
+Y.h   = aperm(run$fcast$fcast, c(2,3,1))
+h     = dim(Y.h)[1]
+
+limits.1    = range(Y.h[,1,])
+point.f     = apply(Y.h[,1,],1,mean)
+interval.f  = apply(Y.h[,1,],1,HDInterval::hdi,credMass=0.90)
+
+x           = seq(from=limits.1[1], to=limits.1[2], length.out=100)
+z           = matrix(NA,h,99)
+for (i in 1:h){
+  z[i,]     = hist(Y.h[i,1,], breaks=x, plot=FALSE)$density
+}
+x           = hist(Y.h[i,1,], breaks=x, plot=FALSE)$mids
+yy          = 1:h
+z           = t(z)
+
+library(plot3D)
+
+theta = 180
+phi   = 15.5
+f4    = persp3D(x=x, y=yy, z=z, phi=phi, theta=theta, xlab="\nrgdp[t+h|t]", ylab="h", zlab="\npredictive densities of rgdp", shade=NA, border=NA, ticktype="detailed", nticks=3,cex.lab=1, col=NA,plot=FALSE)
+perspbox (x=x, y=yy, z=z, bty="f", col.axis="black", phi=phi, theta=theta, xlab="\nrgdp[t+h|t]", ylab="h", zlab="\npredictive densities of rgdp", ticktype="detailed", nticks=3,cex.lab=1, col = NULL, plot = TRUE)
+polygon3D(x=c(interval.f[1,],interval.f[2,h:1]), y=c(1:h,h:1), z=rep(0,2*h), col = "#FF00FF", NAcol = "white", border = NA, add = TRUE, plot = TRUE)
+for (i in 1:h){
+  f4.l = trans3d(x=x, y=yy[i], z=z[,i], pmat=f4)
+  lines(f4.l, lwd=0.5, col="black")
+}
+f4.l1 = trans3d(x=point.f, y=yy, z=0, pmat=f4)
+lines(f4.l1, lwd=2, col="black")
